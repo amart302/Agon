64018deb669f6fa665147ae33b8957281a721069
Ahmed <amartazanov892@gmail.com>
2024-06-08 18:34:17 +0300

    Commit 1

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+const smallProductCards = [
+    {
+        id: 0,
+        imgSrc: "/mainImg/smallProductCardImg1.svg",
+        productName: "4K TV Game Mini Arcade Rocker Console 32GB/64GB",
+        price: "$29.80",
+    },
+    {
+        id: 1,
+        imgSrc: "/mainImg/smallProductCardImg2.svg",
+        productName: "Cancelling Headset Music Sport Deep Bass",
+        price: "$420.00",
+    },
+    {
+        id: 2,
+        imgSrc: "/mainImg/smallProductCardImg3.svg",
+        productName: "T500BT Original Wireless Bluetooth Headphone Deep",
+        price: "$96.50",
+    }
+]
